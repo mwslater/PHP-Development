0c85e03c71d18b16ca8de4d7b32b324121215855
mwslater <mwslater@kent.edu>
2016-03-17 19:19:48 -0400

    Make contact form functional
    Fixed code to have the contact form validate input, send the email, and
    hide the form once email is sent.

--- contact.php
+++ contact.php
@@ -1,59 +1,64 @@
-<?php 
+<?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$name = trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
 	$email = trim(filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL));
 	$phone = trim(filter_input(INPUT_POST,"phone",FILTER_SANITIZE_NUMBER_INT));
 	$message = trim(filter_input(INPUT_POST,"message",FILTER_SANITIZE_SPECIAL_CHARS));
-	
+
 	if ($name == "" || $email == "" || $phone == "" || $message == "") {
 		$error_message = "Please fill in the required fields: Name, Email, Phone Number, and Message";
 	}
+	
+	if (strlen($phone) < 10) {
+		$error_message = "Please enter a valid phone number.";
+	}
+	
 	if (!isset($error_message) && $_POST["address"] != "") {
 		$error_message = "Bad form input";
 	}
 	
-	require("inc/phpmailer/class.phpmailer.php");
-	
+	require("inc/PHPMailerAutoload.php");
+
 	$mail = new PHPMailer;
-	
+
 	if (!isset($error_message) && !$mail->ValidateAddress($email)) {
 		$error_message = "Invalid Email Address";
 	}
-	
+
 	if (!isset($error_message)) {
 		$email_body = "";
 		$email_body .= "Name " . $name . "\n";
 		$email_body .= "Email " . $email . "\n";
 		$email_body .= "Phone " . $phone . "\n";
 		$email_body .= "Message " . $message . "\n";
-		
+
 		$mail->isSMTP();                                      // Set mailer to use SMTP
 		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 		$mail->SMTPAuth = true;                               // Enable SMTP authentication
 		$mail->Username = 'mwslater@kent.edu';                 // SMTP username
 		$mail->Password = '9F10F60f';                           // SMTP password
 		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-		$mail->Port = 587;                                    // TCP port to connect to		
+		$mail->Port = 587;                                    // TCP port to connect to
 		$mail->setFrom($email, $name);
 		$mail->addAddress('mwslater@kent.edu', 'Matt Slater');     // Add a recipient
-		
+
 		$mail->isHTML(false);                                  // Set email format to HTML
 
 		$mail->Subject = 'Wreath Inquiry from ' . $name;
 		$mail->Body    = $email_body;
-		
+
 		if($mail->send()) {
-			header("location:suggest.php?status=thanks");
+			header("location:contact.php?status=thanks");
 			exit;
 		}
 		$error_message = 'Message could not be sent.';
 		$error_message .= 'Mailer Error: ' . $mail->ErrorInfo;
 	}
-	
+
 }
 
- 
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -86,16 +91,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <section id="primary">
         <h3>General Information</h3>
 		<?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
-			echo "<p>Thanks for the email!  I$rsquo;ll get back to you shortly!</p>";			
-		}  else { 
+			echo "<p>Thanks for the email!  I will get back to you shortly!</p>";
+		}  else  
 			if (isset($error_message)) {
-				echo "<p class='message'>".$error_message . "</p>";
+				echo "<p class='message'>".$error_message . "</p>
+					  <p>Use the back button to go back and try again.</p>";
 			} else {
 				echo "<p>Here are the ways to contact me if you have any questions or you would like to order a wreath.</p>
 					<p>Please only use phone contact for urgent inquries.  Otherwise, please fill out the form below and I will contact you to answer your questions and/or go over your design requirements.</p>";
-			}
+			
 		?>
-        
+
             <form action="contact.php" method="POST" enctype="multipart/form-data">
                 <table>
                         <tr>
@@ -107,7 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <td><input type="text" id="email" name="email" value="<?php if (isset($email)) { echo $email; } ?>" /></td> /></td>
                         </tr>
                         <tr>
-                            <th><label for="phone">Phone Number (required)</label></th>
+                            <th><label for="phone">Phone Number with area code (required)</label></th>
                             <td><input type="text" id="phone" name="phone" value="<?php if (isset($phone)) { echo $phone; } ?>" /></td> /></td>
                         </tr>
                         <tr>
@@ -122,6 +128,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </table>
                 <input type="submit" value="Send" />
             </form>
+            <?php } ?>
       </section>
       <section id="secondary">
         <h3>Contact details</h3>
@@ -130,12 +137,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <li class="mail"><a href="mailto:alyabe4@aol.com?subject=Wreath Inquiry">alyabe4@aol.com</a></li>
          </ul>
       </section>
-      
+
       <footer>
         <a href="https://www.pinterest.com/alyanab15/" tabindex="9"><img src="https://cdn-images.mailchimp.com/icons/social-block-v2/color-pinterest-48.png" alt="Pinterest Logo" class="social-icon"></a>
         <a href="https://www.facebook.com/wreathsbyyana" tabindex="10"><img src="https://cdn-images.mailchimp.com/icons/social-block-v2/color-facebook-48.png" alt="Facebook Logo" class="social-icon"></a>
         <p>&copy; 2015 Wreaths by Yana.</p>
       </footer>
     </div>
-  </body>  
+  </body>
 </html>
