71a01d50783c64d33a19ea309c7b8029649b6b41
mwslater <mwslater@kent.edu>
2016-03-25 08:23:02 -0400

    Completed working project
    Database connection tested and working.  Removed the .html versions of
    the site files.  Edited the email contact info and email account used to
    send messages from the form.

--- contact (2).php
+++ contact (2).php
@@ -1,88 +1,3 @@
-<?php
-
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "wreath_contacts";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$name = trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
-	$email = trim(filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL));
-	$phone = trim(filter_input(INPUT_POST,"phone",FILTER_SANITIZE_NUMBER_INT));
-	$message = trim(filter_input(INPUT_POST,"message",FILTER_SANITIZE_SPECIAL_CHARS));
-
-	if ($name == "" || $email == "" || $phone == "" || $message == "") {
-		$error_message = "Please fill in the required fields: Name, Email, Phone Number, and Message";
-	}
-	
-	if (strlen($phone) < 10) {
-		$error_message = "Please enter a valid phone number.";
-	}
-	
-	if (!isset($error_message) && $_POST["address"] != "") {
-		$error_message = "Bad form input";
-	}
-	
-	require("inc/PHPMailerAutoload.php");
-
-	$mail = new PHPMailer;
-
-	if (!isset($error_message) && !$mail->ValidateAddress($email)) {
-		$error_message = "Invalid Email Address";
-	}
-
-	if (!isset($error_message)) {
-		$email_body = "";
-		$email_body .= "Name " . $name . "\n";
-		$email_body .= "Email " . $email . "\n";
-		$email_body .= "Phone " . $phone . "\n";
-		$email_body .= "Message " . $message . "\n";
-
-		$mail->isSMTP();                                      // Set mailer to use SMTP
-		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'wreathsbyyana@gmail.com';                 // SMTP username
-		$mail->Password = 'wreathemail';                           // SMTP password
-		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-		$mail->Port = 587;                                    // TCP port to connect to
-		$mail->setFrom($email, $name);
-		$mail->addAddress('mwslater@kent.edu', 'Matt Slater');     // Add a recipient
-		$mail->addAddress('wreathsbyyana@gmail.com', 'Wreaths by Yana');
-
-		$mail->isHTML(false);                                  // Set email format to HTML
-
-		$mail->Subject = 'Wreath Inquiry from ' . $name;
-		$mail->Body    = $email_body;
-		
-		$sql = "INSERT INTO `wreath_contacts`.`contacts` (`name`, `email`, `phone`) VALUES ('$name', '$email', '$phone');";
-				
-		if ($conn->query($sql) === TRUE) {
-    		echo "New record created successfully";
-		} else {
-    		echo "Error: " . $sql . "<br>" . $conn->error;
-		}
-
-		$conn->close();
-		
-		if($mail->send()) {
-			header("location:contact.php?status=thanks");
-			exit;
-		}
-		$error_message = 'Message could not be sent.';
-		$error_message .= 'Mailer Error: ' . $mail->ErrorInfo;
-	}
-
-}
-
-
-?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -113,18 +28,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div id="wrapper">
       <section id="primary">
         <h3>General Information</h3>
-		<?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
-			echo "<p>Thanks for the email!  I will get back to you shortly!</p>";
-		}  else  
-			if (isset($error_message)) {
-				echo "<p class='message'>".$error_message . "</p>
-					  <p>Use the back button to go back and try again.</p>";
-			} else {
-				echo "<p>Here are the ways to contact me if you have any questions or you would like to order a wreath.</p>
-					<p>Please only use phone contact for urgent inquries.  Otherwise, please fill out the form below and I will contact you to answer your questions and/or go over your design requirements.</p>";
-			
-		?>
-
+		<p>Here are the ways to contact me if you have any questions or you would like to order a wreath.</p>
+					<p>Please only use phone contact for urgent inquries.  Otherwise, please fill out the form below and I will contact you to answer your questions and/or go over your design requirements.</p>
             <form action="contact.php" method="POST" enctype="multipart/form-data">
             		<table>
                         <tr>
@@ -159,13 +64,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </table>
                 <input type="submit" value="Send" />
             </form>
-            <?php } ?>
-      </section>
+                  </section>
       <section id="secondary">
         <h3>Contact details</h3>
         <ul class="contact-info">
           <li class="phone"><a href="tel:216-870-8222">216-870-8222</a></li>
-          <li class="mail"><a href="mailto:wreathsbyyana@gmail.com?subject=Wreath Inquiry">wreathsbyyana@gmail.com</a></li>
+          <li class="mail"><a href="mailto:alyabe4@aol.com?subject=Wreath Inquiry">alyabe4@aol.com</a></li>
          </ul>
       </section>
 
---
+++ wreath_contacts.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: wreath_contacts
+-- ------------------------------------------------------
+-- Server version	5.6.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contacts` (
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for storing contact info from wreath site.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacts`
+--
+
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+INSERT INTO `contacts` VALUES ('Jean-Luc Picard','jpicard2@starfleet.mil','2220256547'),('John Doe','john@doe.net','5555555555'),('Miles O&#39;Brien','mobrien@starfleet.mil','9876543210'),('Will Riker','will@riker.com','7894561230');
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-25  8:08:18
